7c6bd5867b7589a041f95dc856d12a2728980269
beawkiattisak <bbeawz.2550@gmail.com>
2024-02-18 12:30:23 +0700

    Template

---
+++ Template.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+
+using namespace std;
+
+#define int long long
+#define arr array
+#define endl "\n"
+#define pb push_back
+#define fi first
+#define se second
+#define vt vector
+#define all(x) (x).begin(), (x).end()
+
+const int MOD = 1e9 + 7;
+const int INF = LLONG_MAX;
+
+void solve() {
+
+  ;
+}
+
+int32_t main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  solve();
+
+  return 0;
+}
