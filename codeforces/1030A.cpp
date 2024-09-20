aedd8d0c8a3b8cd97bed76a23b9be9cad80356c3
beawkiattisak <beawkiattisak@gmail.com>
2024-09-20 18:58:49 +0700

    1030A

--- codeforces/1030A.cpp
+++ codeforces/1030A.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-   	cin.tie(nullptr)->ios::sync_with_stdio(false);
+    cin.tie(nullptr)->ios::sync_with_stdio(false);
 
     int t, n; cin >> t;
     bool flag = false;
